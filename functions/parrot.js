e808982ba1292c91a964bd18609750ed074ce060
Soudry Gabriel <gabriel.soudry@student-cs.fr>
2020-10-27 11:07:52 +0100

    message2

--- functions/parrot.js
+++ functions/parrot.js
@@ -3,12 +3,11 @@ const querystring = require("querystring");
 exports.handler = async function (event, context) {
   const requestBody = JSON.parse(event.body);
   const message = requestBody.message;
-  let body = event.queryStringParameters;
 
-  if (body.name) {
+  if (message) {
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: body.name }),
+      body: JSON.stringify({ message: message.name }),
     };
   } else {
     return {
